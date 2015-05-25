6b043aa553299610a32f192e5ed8cfa9c3c9bd82
Renata Technologies Pvt Ltd <renata@Renatas-Mac-mini.local>
2015-05-25 14:45:35 +0530

    Initial Commit

---
+++ TestAppforGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppforGit
+//
+//  Created by Renata Technologies Pvt Ltd on 25/05/15.
+//  Copyright (c) 2015 Renata Technologies Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
